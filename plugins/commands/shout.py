b5178292260d5ab90a1b6cd47171c6e7982e44d9
apple_qaq <apple_qaq@outlook.com>
2023-10-01 23:59:20 +0800

    new: .shout can be canceled

--- plugins/commands/shout.py
+++ plugins/commands/shout.py
@@ -4,10 +4,17 @@ import time
 import api.gocqhttp
 import data.log
 
+is_send = True
+
 
 def run(message):
+    global is_send
     if message.is_private_message() is False:
         return ""
+    if message.message == ".shout stop":
+        is_send = False
+        data.log.get_logger().info(f"上一条 shout 停止发送")
+        return "⇒ 已经停止发送"
     send_message: str = message.message.replace(".shout", "", 1)
 
     if send_message[0] == "\n":
@@ -22,15 +29,17 @@ def run(message):
         already_join_group_list += [i["group_id"]]
 
     api.gocqhttp.send_private_msg(user_id=message.user_id,
-                                  message=f"⇒ 请等待 60 秒，如果有改变请迅速前往服务器关闭进程并重启！\n\n"
+                                  message=f"⇒ 请等待 60 秒，如果有改变请迅速发送\".shout stop\"\n\n"
                                           f"消息为:\n"
                                           f"{send_message}")
 
     time.sleep(60)
-    api.gocqhttp.send_private_msg(user_id=message.user_id, message="⇒ 开始发送")
-    for i in already_join_group_list:
-        time.sleep(random.randint(1, 2))
-        api.gocqhttp.send_group_msg(i, send_message)
 
-    api.gocqhttp.send_private_msg(user_id=message.user_id, message="→ 全部发送完毕！！")
-    data.log.get_logger().info(f"全部发送完毕\n消息:\n{send_message}")
+    if is_send:
+        api.gocqhttp.send_private_msg(user_id=message.user_id, message="⇒ 开始发送")
+        for i in already_join_group_list:
+            time.sleep(random.randint(1, 2))
+            api.gocqhttp.send_group_msg(i, send_message)
+
+        api.gocqhttp.send_private_msg(user_id=message.user_id, message="→ 全部发送完毕！！")
+        data.log.get_logger().info(f"全部发送完毕\n消息:\n{send_message}")
